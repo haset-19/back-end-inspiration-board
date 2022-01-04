cc7e60e9bfdf229cae15a0b51cf74ac67cb5a07b
roza ajema <mentoroza@yahoo.com>
2022-01-04 10:10:47 -0800

    adding foreign key

--- app/models/card.py
+++ app/models/card.py
@@ -4,7 +4,9 @@ class Card (db.Model):
   card_id = db.Column(db.Integer, primary_key = True, autoincrement=True)
   message = db.Column(db.String)
   likes_count = db.Column(db.Integer)
+
   board_id = db.Column(db.Integer, db.ForeignKey('board.board_id'), nullable=True)
+
   board = db.relationship("Board", back_populates="cards")
 
   def to_dict(self):
